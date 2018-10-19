0baeaf7a3b5361772e7dd47589eed88c760dfb05
“theodorewahle” <theodorewahle@gmail.com>
2018-10-18 23:59:06 -0400

    stubbed out search bar with some reactstrap components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap'
 import logo from './logo.svg';
+import Search from './screens/Search'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <Container>
-      <Row>
-        <Col>
-        <h1>map</h1>
-        </Col>
-        <Col>
-        <h1>sidebar</h1>
-        </Col>
+        <Row>
+          <Col className="bg-black" xs="8">
+            <h1>filter</h1>
+          </Col>
+          <Col className="bg-orange" xs="4">
+            <Search/>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="bg-black" xs="8">
+            <h1>map</h1>
+          </Col>
+          <Col className="bg-orange" xs="4">
+            <h1>sidebar</h1>
+          </Col>
         </Row>
       </Container>
     );
---
+++ src/screens/Search.js
@@ -0,0 +1,20 @@
+import React, { Component} from 'react'
+import { Input, InputGroup, Button, Row, Col } from 'reactstrap'
+
+class Search extends Component {
+  render() {
+    return (
+      <Row>
+      <Col>
+      <h3>Search Members</h3>
+      <InputGroup>
+        <Input placeholder="last name" />
+      </InputGroup>
+      <Button>Search</Button>
+      </Col>
+      </Row>
+    )
+  }
+}
+
+export default Search
